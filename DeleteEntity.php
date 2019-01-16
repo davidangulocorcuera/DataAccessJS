72e2f32f032e2bb3367a5e45cd44cd35c83d299a
milw0rm55 <sebastiantace@gmail.com>
2019-01-16 19:53:12 +0100

    write entity

---
+++ DeleteEntity.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: milw0rm
+ * Date: 15/01/19
+ * Time: 12:09
+ */
---
+++ UpdateEntity.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: milw0rm
+ * Date: 16/01/19
+ * Time: 19:39
+ */
